cada45f1656962df8bdf2fb7739be8e44bb68382
Patrick Alexander <patrick_alexander@mail.com>
2019-08-31 00:25:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   array.include?(value_to_find) do |value|
     if value == value_to_find
-    return array.index(value)
+    array.index(value)
     end
   end
 end
