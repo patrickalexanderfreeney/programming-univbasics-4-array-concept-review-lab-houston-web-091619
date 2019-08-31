551d7696d0addcc74efdc69950b8d32b1f4ad789
Patrick Alexander <patrick_alexander@mail.com>
2019-08-31 00:01:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,3 +5,11 @@ def find_element_index(array, value_to_find)
       array[index] 
   end  
 end
+
+def find_max_value(array)
+  # Add your solution here
+end
+
+def find_min_value(array)
+  # Add your solution here
+end
