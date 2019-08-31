cf30f00a7274fadddfd61fdece18fa4740b61ed7
Patrick Alexander <patrick_alexander@mail.com>
2019-08-31 01:16:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
-  
+  counter = 0
+  while counter < array.length do
+    if array[counter] == value_to_find
+      return counter 
+  end
 end
 
 def find_max_value(array)
