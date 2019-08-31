5af5acad4af9fe0255790fa35a434d8f57cc1d29
Patrick Alexander <patrick_alexander@mail.com>
2019-08-31 01:16:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,16 +13,3 @@ end
 def find_min_value(array)
   array.min
 end
-
-def find_element_index(array, value_to_find)
-  array.index(value_to_find)
-  
-end
-  counter = 0
-  while counter < array.length do
-    if array[counter] == value_to_find
-      return counter 
-  end
-en
-
-array.index(value_to_find)
